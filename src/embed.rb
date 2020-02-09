bb2afa44973777ec9c62b715427b517344d59b8a
asahina-dev <0.0.nn.ai@gmail.com>
2020-02-09 23:25:37 +0900

    feat:ライブラリパスを変更/コメントを追加

--- src/embed.rb
+++ src/embed.rb
@@ -1,11 +1,15 @@
 require 'ffi'
 
-module Hello
+# FFI::Libraryモジュールを拡張し、ffi_libを呼び出す
+  module Hello
   extend FFI::Library
-  ffi_lib 'target/release/libembed.so'
+  # リリースビルドで生成したライブラリからffi_libを呼び出す
+  ffi_lib 'target/release/libembed.dylib'
+  # Rustのprocess()を同じ名前のRuby関数に接続する
   attach_function :process, [], :void
 end
 
+# 実際のRustの呼び出し
 Hello.process
 
 puts 'done!'
